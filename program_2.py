d3545ca8f4fc327317276df37469d2d6f87575aa
M-Court <mcourt@students.unwsp.edu>
2024-09-12 22:59:13 -0500

    Update program_2.py

--- program_2.py
+++ program_2.py
@@ -1,13 +1,10 @@
-
-def average_age():
-    # Get User Input
-
-
-    # Sum ages
-
-    # Average the ages
-
-    # Print the results
-
-# Line which calls the above function.
-average_age()
+#ask for five ages
+age1 = int(input("Enter age 1: "))
+age2 = int(input("Enter age 2: ")) 
+age3 = int(input("Enter age 3: "))
+age4 = int(input("Enter age 4: "))
+age5 = int(input("Enter age 5: ")) 
+#average ages
+total = (age1 + age2 + age3 + age4 + age5) / 5
+#print average
+print("The average of the five ages is", total)
